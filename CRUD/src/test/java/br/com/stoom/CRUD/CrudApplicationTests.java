05be88beb09e272e374e9b834120eeb89aed0343
pddec <73551981+pddec@users.noreply.github.com>
2021-05-10 14:03:30 -0300

    Dock

--- CRUD/src/test/java/br/com/stoom/CRUD/CrudApplicationTests.java
+++ CRUD/src/test/java/br/com/stoom/CRUD/CrudApplicationTests.java
@@ -1,25 +1,22 @@
 package br.com.stoom.CRUD;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.stoom.CRUD.dto.AddressDTO;
 import br.com.stoom.CRUD.dto.AddressDTO.BuilderAddressDTO;
 import br.com.stoom.CRUD.models.Address;
 import br.com.stoom.CRUD.services.interfaces.Services;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest()
 @ActiveProfiles("test")
 class CrudApplicationTests {
